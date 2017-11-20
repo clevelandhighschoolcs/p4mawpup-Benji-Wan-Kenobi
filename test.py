b1670c87a25af905768b5a8a503f49d96795b96e
Ben McFarland <benrmcfarland@gmail.com>
2017-11-20 15:04:58 -0800

    Added "test.py"

---
+++ test.py
@@ -0,0 +1,35 @@
+import urllib2
+import time
+
+def WebData():
+	
+	link = raw_input ("Paste URL here:")
+	sec = int(raw_input("How many seconds do you want to test for?"))
+	
+	URL = urllib2.urlopen(link)
+	data = URL.read()
+	datastring = str(data)
+	lengthB = len(datastring)
+	
+	timeT = 0
+	
+	while timeT < sec:
+	
+		URL = urllib2.urlopen(link)
+		data = URL.read()
+		datastring = str(data)
+		lengthA = len(datastring)
+		
+		print("Testing...")
+		if lengthB != lengthA:
+			print ("The site has changed!")
+			break
+		else:
+			lengthA = lengthB
+			timeT += 1
+			time.sleep(1)
+			
+		if timeT == sec:
+			print ("No changes were made.")
+	
+WebData()
