29e1d1f22bf3dec4c6197016ed800d4988144391
Ben McFarland <benrmcfarland@gmail.com>
2017-12-11 15:12:06 -0800

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,11 +1,5 @@
 import urllib2
 import time
-from twilio.rest import Client
-
-account_sid = 'ACa7889d2092ab17c8907d142adf8a915b'
-auth_token = 'b9ea03d04522dc1051bc991bf757494b'
-twilio_phone_number = '+14042387455'
-my_phone_number = "+16787932030"
 
 def WebData():
 	
@@ -28,14 +22,7 @@ def WebData():
 		
 		print("Testing...")
 		if lengthB != lengthA:
-			body = "The site has changed!"
-			client = Client(account_sid, auth_token)
-			client.messages.create(
-				body=body,
-				to=my_phone_number,
-				from_=twilio_phone_number
-			)
-			print("Change detected, text sent")
+			print("The site has changed!")
 			break
 		else:
 			lengthA = lengthB
